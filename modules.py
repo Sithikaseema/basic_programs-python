15e3db0b428a57c45061c1b259baac306d422fd4
Sithikaseema <61896234+Sithikaseema@users.noreply.github.com>
2020-10-18 08:46:21 -0700

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,18 @@
+#modules
+
+#random
+import random
+print("random :",random.randint(2,7))
+
+#math
+import math
+print("value of pi: ",math.pi)
+print("sqrt of 144 is: ",math.sqrt(144))
+
+
+#datetime
+import datetime
+print("current date and time is : ",datetime.datetime.now())
+
+
+
